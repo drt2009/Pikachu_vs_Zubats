e03aaf36b7f2b7f4d1ef6f3cf8071ffccd704c1f
Don Tobias <dtobiasalmont09@gmail.com>
2016-08-02 19:41:19 -0400

    Created High Score class.  Also muted the music for testing, It was annoying

--- PikachuVsZubats/Game.py
+++ PikachuVsZubats/Game.py
@@ -10,6 +10,7 @@ from Sprites.Pikachu import Pikachu
 from Sprites.Zubat import Zubat
 from Sprites.Bullet import Bullet
 from Background import Background
+from HighScore import HighScore
 
 #lastHit=1 for right and 0 for left
 lastHit=1
@@ -72,56 +73,7 @@ def Game():
                         askForInitials()
                         initialsEntered=True
                         break
-
-        #Opens the HighScore file for reading
-        highScore=open('HighScore.txt','r')
-
-        fileLine=0
-        firstline=""
-        highScores={}
-        #Reads in the file
-        for line in highScore:
-            #Reads in the header line
-            if(fileLine==0):
-                firstline=line
-                fileLine+=1
-                continue
-            #Gets the name,level, and score of the players
-            temp=[]
-            #splits the info into 3 different parts then save them to
-            #the dictionary
-            split=line.split("\t\t")
-            temp.append(split[1])
-            temp.append(split[2])
-            temp.append(int(split[3]))
-            highScores[split[0]]=temp
-
-        #closes the read on the highscore list
-        highScore.close()
-        playerStats=[]
-        #makes a list of the current player stats so they can be comparied against
-        #the list
-        playerStats.append(initials)
-        playerStats.append(str(player.level))
-        playerStats.append(player.pikaScore)
-
-        #goes through the dictionary and sees where the current player needs to get added
-        #then will bump the person at the spot move down one
-        for x in range((len(highScores))):
-            if (highScores[str(len(highScores)-x)+")"][2]<player.pikaScore):
-                highScores[str(len(highScores)-x+1)+")"]=highScores[str(len(highScores)-x)+")"]
-                highScores[str(len(highScores)-x)+")"]=playerStats
-
-        #Opens a high score list for writing that will delete the old list
-        #then replace it with the new names that go on it
-        highScore=open('HighScore.txt','w')
-        highScore.write(firstline)
-        for x in range(1,11):
-            highScore.write(str(x)+")\t\t"+highScores[str(x)+")"][0]+"\t\t"\
-                            +highScores[str(x)+")"][1]+"\t\t"+str(highScores[str(x)+")"][2])+"\t\t\n")
-
-        #closes the high score list
-        highScore.close()
+        HighScore(initials, player)
 
         while playAgain==False:
             myfont = pygame.font.Font(None, 36)
@@ -268,7 +220,7 @@ def Game():
     bulletListLeft= pygame.sprite.RenderPlain()
     zubats=pygame.sprite.RenderPlain()
 
-   
+
 
     while True:
 
